a5b7ac35a1ed3a11a16567ab68c98b97e9f538ce
lukemew <123739531+lukemew@users.noreply.github.com>
2023-09-14 09:15:28 -0300

    Iniciando o código

---
+++ app/script.js
@@ -0,0 +1,15 @@
+const botao = document.querySelector('#enviar')
+
+
+async function gerarCep(cep){
+try {
+    var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    var response = await consultaCep.json()
+    console.log(response.localidade)
+
+} catch (error) {
+    console.error("Não foi possível conseguir o CEP:", error)
+}
+}
+
+gerarCep('63870000')
