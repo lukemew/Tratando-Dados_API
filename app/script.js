e9a11cd73c66ea5faf88c766f49cd9e69fbb0b15
lukemew <123739531+lukemew@users.noreply.github.com>
2023-09-14 10:49:03 -0300

    Finalizando o processo de preenchimento automatico

--- app/script.js
+++ app/script.js
@@ -1,15 +1,33 @@
-const botao = document.querySelector('#enviar')
-
+const campoCep = document.querySelector('#cep')
+const cidadeInput = document.querySelector('#cidade')
+const endereçoInput = document.querySelector('#endereco')
+const bairroInput = document.querySelector('#bairro')
+const estadoSelect = document.querySelector('#estado')
+const complementoInput = document.querySelector('#complemento')
 
 async function gerarCep(cep){
 try {
     var consultaCep = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
     var response = await consultaCep.json()
-    console.log(response.localidade)
+    console.log(response)
+   
+    cidadeInput.value = response.localidade
+    endereçoInput.value = response.logradouro
+    bairroInput.value = response.bairro
+    estadoSelect.value = response.uf
+    complementoInput.value = response.complemento
+
 
 } catch (error) {
     console.error("Não foi possível conseguir o CEP:", error)
 }
 }
 
-gerarCep('63870000').
+campoCep.addEventListener('input', () => {
+    
+    const cepDigitado = campoCep.value
+    
+    if(cepDigitado.length === 8){
+    gerarCep(cepDigitado)
+    }
+})
